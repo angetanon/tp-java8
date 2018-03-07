a7eb67b8c58a3d71dc3d5367df5dbd2d65f3e9cb
ange <abdjinan@yahoo.fr>
2018-03-07 10:37:35 +0100

    Functions

--- src/test/java/java8/ex03/Function_03_Test.java
+++ src/test/java/java8/ex03/Function_03_Test.java
@@ -16,8 +16,16 @@ public class Function_03_Test {
     // TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
     // TODO l'age de l'enfant est 0
     // TODO le mot de passe de l'enfant est null
-    BinaryOperator<Person> makeAChild = null;
-    //  end::makeAChild[]
+    BinaryOperator<Person> makeAChild = (f,m)-> {
+    	
+    	Person p = new Person();
+    	
+    	p.setLastname(f.getLastname());
+    	p.setFirstname(f.getFirstname() + " " + m.getFirstname());
+    	p.setAge(0);
+    	p.setPassword(null);
+    	return p;
+    };
 
 
     @Test
@@ -27,7 +35,7 @@ public class Function_03_Test {
         Person mother = new Person("Aline", "Lebreton", 22, "alino");
 
         // TODO compléter le test pour qu'il soit passant
-        Person child = null;
+        Person child = makeAChild.apply(father, mother);
 
         assert child.getFirstname().equals("John Aline");
         assert child.getLastname().equals("France");
--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -22,7 +22,8 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+    	Person p = supplier.get();
+        return "[age=" + p.getAge()+ "]";
     }
     // end::formatAge[]
 
@@ -30,7 +31,9 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+    	
+    	
+        String result = formatAge(()-> new Person("","",35,""));
 
         assert result.equals("[age=35]");
     }
@@ -42,7 +45,7 @@ public class Function_06_Test {
         expectedException.expectMessage("require non null object");
 
         // TODO compléter le test unitaire pour qu'il soit passant
-        Supplier<String> supplier = null;
+        Supplier<String> supplier = ()->"require non null object";
 
         // Avec un paramètre null, cette méthode déclenche un NullPointerException
         Objects.requireNonNull(null, supplier);
